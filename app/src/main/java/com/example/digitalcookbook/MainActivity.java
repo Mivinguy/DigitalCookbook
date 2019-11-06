ea8fc3cf5296131a852855156dc0009fb71e6372
Camden Christopherson <cchrist2@ucsc.edu>
2019-11-05 20:08:33 -0800

    Dynamic categories

--- app/src/main/java/com/example/digitalcookbook/MainActivity.java
+++ app/src/main/java/com/example/digitalcookbook/MainActivity.java
@@ -1,64 +1,121 @@
 package com.example.digitalcookbook;
-
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.gridlayout.widget.GridLayout;
-
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseReference mDatabase;
+    ArrayList<String> category_names = new ArrayList<>();
     GridLayout mainGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        // Set onClickListeners for category cards
         mainGrid = findViewById(R.id.grid);
-        setSingleEvent(mainGrid);
 
+        // Firebase
+        // Get a reference to database
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        // Floating Action Button
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        // Read in categories and assign to categories list
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("NAMES", dataSnapshot.getKey());
+                category_names.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        // Attach a listener to read the data on change, updates recyclerview
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Set onClickListeners for category cards and display names
+                setSingleEvent(mainGrid);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
     }
 
     private void setSingleEvent(GridLayout mainGrid) {
+
+        // Set the TextView layoutParams
+        // Print category names
+        // Initialize a new TextView to put in CardView
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params.leftMargin = 16;
         // Set onClickListeners for each CardView
         for (int i = 0; i < mainGrid.getChildCount(); i++) {
             CardView cardView = (CardView)mainGrid.getChildAt(i);
+
+            // Set the TextView dynamically
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(params);
+            tv.setText(category_names.get(i));
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24);
+            tv.setTextColor(Color.BLACK);
+            tv.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
+            cardView.addView(tv);
+
             final int selected = i;
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(MainActivity.this, CategoryRecipes.class);
-                        intent.putExtra("Category",selected);
-//
-//                    if (selected == 0) {
-//                        intent.putExtra("Category",selected);
-//                    } else if (selected == 1) {
-//                        intent.putExtra("Category",selected);
-//                    } else if (selected == 2) {
-//                        intent.putExtra("Category",selected);
-//                    } else if (selected == 3) {
-//                        intent.putExtra("Category",selected);
-//                    }
+                    intent.putExtra("com.example.digitalcookbook.Category",selected);
                     startActivity(intent);
                 }
             });
--- app/src/main/java/com/example/digitalcookbook/RecipeListViewHolder.java
+++ app/src/main/java/com/example/digitalcookbook/RecipeListViewHolder.java
@@ -2,10 +2,7 @@ package com.example.digitalcookbook;
 
 import android.view.View;
 import android.widget.TextView;
-
-import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
 
 public class RecipeListViewHolder extends RecyclerView.ViewHolder {
     TextView recipe_title;
