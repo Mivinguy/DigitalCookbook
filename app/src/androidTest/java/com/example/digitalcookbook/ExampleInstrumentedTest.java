31bdb7895338b47c4628d22780a19306912d9ba4
dfiresti <56855809+dfiresti@users.noreply.github.com>
2019-11-26 23:49:17 -0800

    Added unit tests

--- app/src/androidTest/java/com/example/digitalcookbook/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/digitalcookbook/ExampleInstrumentedTest.java
@@ -7,6 +7,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import androidx.test.rule.ActivityTestRule;
+
 
 import static org.junit.Assert.*;
 
@@ -21,7 +27,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
         assertEquals("com.example.digitalcookbook", appContext.getPackageName());
     }
 }
