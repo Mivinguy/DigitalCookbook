7df2ba252b6bdb49fb40db5d24986fdef2a9a4b0
Damien-Machado <47127792+Damien-Machado@users.noreply.github.com>
2019-12-01 12:13:35 -0800

    quick formatting

--- app/src/main/java/com/example/digitalcookbook/Category.java
+++ app/src/main/java/com/example/digitalcookbook/Category.java
@@ -1,31 +1,30 @@
 package com.example.digitalcookbook;
+
 import android.graphics.drawable.Drawable;
 import java.util.ArrayList;
 
 public class Category {
 
-    private String mName;
-    private Drawable image;
-    private ArrayList<Recipe> mRecipes;
+  private String mName;
+  private Drawable image;
+  private ArrayList<Recipe> mRecipes;
 
-    public Category() {
-    }
+  public Category() {}
 
-    public Category(String name) {
-        this.mName = name;
-    }
+  public Category(String name) {
+    this.mName = name;
+  }
 
-    public Category(String name, ArrayList<Recipe> recipes) {
-        this.mName = name;
-        this.mRecipes= recipes;
-    }
+  public Category(String name, ArrayList<Recipe> recipes) {
+    this.mName = name;
+    this.mRecipes = recipes;
+  }
 
+  public String getmName() {
+    return mName;
+  }
 
-    public String getmName() {
-        return mName;
-    }
-
-    public ArrayList<Recipe> getmRecipes() {
-        return mRecipes;
-    }
+  public ArrayList<Recipe> getmRecipes() {
+    return mRecipes;
+  }
 }
--- app/src/main/java/com/example/digitalcookbook/RecipeView.java
+++ app/src/main/java/com/example/digitalcookbook/RecipeView.java
@@ -30,282 +30,300 @@ import java.lang.StrictMath;
 import android.view.GestureDetector;
 
 public class RecipeView extends AppCompatActivity implements SensorEventListener {
-    Recipe recipe;
-    HashMap<String, String> ingredients = new HashMap<>();
-    //HashMap<String, String> steps = new HashMap<>();
+  Recipe recipe;
+  HashMap<String, String> ingredients = new HashMap<>();
+  // HashMap<String, String> steps = new HashMap<>();
 
-    TextToSpeech currentStepTTS;
-    View ingredientSide;
-    View stepsSide;
-    Boolean showBack = false;
-    Button readNextStep;
-    Button readPrevStep;
-    Button readThisStep;
+  TextToSpeech currentStepTTS;
+  View ingredientSide;
+  View stepsSide;
+  Boolean showBack = false;
+  Boolean isFavorited = false;
+  Button readNextStep;
+  Button readPrevStep;
+  Button readThisStep;
+  Button favoriteButton;
+  Button unfavoriteButton;
 
-    List<String> steps = new ArrayList<String>();
-    int currentStepNum = 0;
+  List<String> steps = new ArrayList<String>();
+  int currentStepNum = 0;
 
-    SensorManager sensorManager;
-    Sensor accelerometer;
-    Vibrator vibrator;
-    private final String TAG = "GestureDemo";
-    private GestureDetectorCompat mDetector;
-    private static final int SHAKE_THRESHOLD = 12;
-    long lastUpdate;
-    float threshold;
-    float prevX;
-    float prevY;
-    float prevZ;
+  SensorManager sensorManager;
+  Sensor accelerometer;
+  Vibrator vibrator;
+  private final String TAG = "GestureDemo";
+  private GestureDetectorCompat mDetector;
+  private static final int SHAKE_THRESHOLD = 12;
+  long lastUpdate;
+  float threshold;
+  float prevX;
+  float prevY;
+  float prevZ;
 
-    boolean buttonPressed = false;
+  boolean buttonPressed = false;
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_recipe);
+
+    readNextStep = (Button) findViewById(R.id.readNextStep);
+    readPrevStep = (Button) findViewById(R.id.readPrevStep);
+    readThisStep = (Button) findViewById(R.id.readThisStep);
+    unfavoriteButton = (Button) findViewById(R.id.unfavorite);
+    favoriteButton = (Button) findViewById(R.id.favorite);
+    ingredientSide = (View) findViewById(R.id.front_recipe);
+    stepsSide = findViewById(R.id.back_recipe);
+
+    currentStepTTS =
+        new TextToSpeech(
+            getApplicationContext(),
+            new TextToSpeech.OnInitListener() {
+
+              @Override
+              public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                  currentStepTTS.setLanguage(Locale.UK);
+                }
+              }
+            });
+
+    Intent intent = getIntent();
+    HashMap<String, String> IngHashMap =
+        (HashMap<String, String>) intent.getSerializableExtra("IngHashMap");
+
+    final HashMap<String, String> StepHashMap =
+        (HashMap<String, String>) intent.getSerializableExtra("StepHashMap");
+
+    String imageFileName = (String) intent.getSerializableExtra("ImageFileName");
+
+    TextView ShowIngs = (TextView) findViewById(R.id.ingredientsList);
+    for (Map.Entry<String, String> entry : IngHashMap.entrySet()) {
+      ShowIngs.setText(entry.getValue() + "\n" + ShowIngs.getText());
+    }
+
+    TextView ShowSteps = (TextView) findViewById(R.id.stepsList);
+    int count = 1;
+    String stepsText = "";
+    String textToSpeechText = "";
+    for (Map.Entry<String, String> entry : StepHashMap.entrySet()) {
+      textToSpeechText += entry.getValue() + "\n" + ShowSteps.getText();
+      stepsText += count + ". " + entry.getValue() + "\n";
+      count++;
+    }
+    ShowSteps.setText(stepsText);
+
+    CharSequence charSequence = textToSpeechText;
+    final StringBuilder sb = new StringBuilder(charSequence.length());
+    sb.append(charSequence);
+    String scannerIn = sb.toString();
+    Scanner s = new Scanner(scannerIn);
+    while (s.hasNextLine()) {
+      steps.add(s.nextLine());
+    }
+
+    ImageView img = (ImageView) findViewById(R.id.recipe_image);
+    imageFileName = imageFileName.substring(0, imageFileName.lastIndexOf('.'));
+    int id = getResources().getIdentifier(imageFileName, "drawable", getPackageName());
+    img.setImageResource(id);
+
+    sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+      accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+      threshold = accelerometer.getMaximumRange() / 8;
+    }
+    vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+    mDetector =
+        new GestureDetectorCompat(
+            this,
+            new GestureDetector.OnGestureListener() {
+              @Override
+              public boolean onDown(MotionEvent e) {
+                return false;
+              }
+
+              @Override
+              public void onShowPress(MotionEvent e) {}
+
+              @Override
+              public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+              }
+
+              @Override
+              public boolean onScroll(
+                  MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+              }
+
+              @Override
+              public void onLongPress(MotionEvent e) {}
+
+              @Override
+              public boolean onFling(
+                  MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+              }
+            });
+
+    readNextStep.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            if (currentStepNum < steps.size() - 1) {
+              currentStepNum++;
+              if (currentStepTTS.isSpeaking()) {
+                currentStepTTS.stop();
+              }
+              String toSpeak = steps.get(currentStepNum);
+              currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+          }
+        });
+
+    readPrevStep.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            if (currentStepNum > 0) {
+              currentStepNum--;
+              if (currentStepTTS.isSpeaking()) {
+                currentStepTTS.stop();
+              }
+              String toSpeak = steps.get(currentStepNum);
+              currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+          }
+        });
+
+    readThisStep.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            buttonPressed = true; // turns on sensor
+            if (currentStepTTS.isSpeaking()) {
+              currentStepTTS.stop();
+            }
+            String toSpeak = steps.get(currentStepNum);
+            currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+          }
+        });
+
+    unfavoriteButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            if (isFavorited) {
+              favoriteButton.setVisibility(View.VISIBLE);
+
+            } else {
+              favoriteButton.setVisibility(View.GONE);
+            }
+          }
+        });
+  }
+
+  public void flipper(View v) {
+    currentStepTTS.stop();
+    if (showBack) {
+      ingredientSide.setVisibility(View.VISIBLE);
+      stepsSide.setVisibility(View.GONE);
+      showBack = false;
+    } else {
+      ingredientSide.setVisibility(View.GONE);
+      stepsSide.setVisibility(View.VISIBLE);
+      showBack = true;
+    }
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+  }
+
+  public void onPause() {
+    if (currentStepTTS != null) {
+      currentStepTTS.stop();
+      currentStepTTS.shutdown();
+    }
+    sensorManager.unregisterListener(this);
+    super.onPause();
+  }
+
+  private class MyGestureListener implements GestureDetector.OnGestureListener {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
-
-
-        readNextStep =(Button)findViewById(R.id.readNextStep);
-        readPrevStep =(Button)findViewById(R.id.readPrevStep);
-        readThisStep =(Button)findViewById(R.id.readThisStep);
-        ingredientSide = (View) findViewById(R.id.front_recipe);
-        stepsSide = findViewById(R.id.back_recipe);
-
-        currentStepTTS = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-
-            @Override
-            public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
-                    currentStepTTS.setLanguage(Locale.UK);
-                }
-            }
-        });
-
-        Intent intent = getIntent();
-        HashMap<String, String> IngHashMap = (HashMap<String, String>) intent.getSerializableExtra("IngHashMap");
-
-        final HashMap<String, String> StepHashMap = (HashMap<String, String>) intent.getSerializableExtra("StepHashMap");
-
-        String imageFileName = (String) intent.getSerializableExtra("ImageFileName");
-
-
-        TextView ShowIngs = (TextView)findViewById(R.id.ingredientsList);
-        for(Map.Entry<String,String > entry : IngHashMap.entrySet()){
-            ShowIngs.setText(entry.getValue()+ "\n" + ShowIngs.getText());
-        }
-
-        TextView ShowSteps = (TextView)findViewById(R.id.stepsList);
-        int count = 1;
-        String stepsText = "";
-        String textToSpeechText = "";
-        for(Map.Entry<String,String > entry : StepHashMap.entrySet()){
-            textToSpeechText += entry.getValue()+ "\n" +ShowSteps.getText();
-            stepsText += count + ". " + entry.getValue()+ "\n" +ShowSteps.getText() +'\n';
-            count++;
-        }
-        ShowSteps.setText(stepsText);
-
-        CharSequence charSequence = textToSpeechText;
-        final StringBuilder sb = new StringBuilder(charSequence.length());
-        sb.append(charSequence);
-        String scannerIn =sb.toString();
-        Scanner s = new Scanner(scannerIn);
-        while(s.hasNextLine()) {
-            steps.add(s.nextLine());
-        }
-        
-        ImageView img = (ImageView)findViewById(R.id.recipe_image);
-        imageFileName = imageFileName.substring(0, imageFileName.lastIndexOf('.'));
-        int id = getResources().getIdentifier(imageFileName, "drawable", getPackageName());
-        img.setImageResource(id);
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            threshold = accelerometer.getMaximumRange()/8;
-        }
-        vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
-
-        mDetector = new GestureDetectorCompat(this, new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                return false;
-            }
-        });
-
-        readNextStep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(currentStepNum < steps.size()-1) {
-                    currentStepNum++;
-                    if (currentStepTTS.isSpeaking()) {
-                        currentStepTTS.stop();
-                    }
-                    String toSpeak = steps.get(currentStepNum);
-                    currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
-                }
-            }
-        });
-
-        readPrevStep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(currentStepNum > 0 ) {
-                    currentStepNum--;
-                    if (currentStepTTS.isSpeaking()) {
-                        currentStepTTS.stop();
-                    }
-                    String toSpeak = steps.get(currentStepNum);
-                    currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
-                }
-            }
-        });
-
-        readThisStep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                buttonPressed = true; // turns on sensor
-                if (currentStepTTS.isSpeaking()) {
-                    currentStepTTS.stop();
-                }
-                String toSpeak = steps.get(currentStepNum);
-                currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
-                }
-
-        });
-    }
-
-    public void flipper(View v) {
-        currentStepTTS.stop();
-        if (showBack) {
-            ingredientSide.setVisibility(View.VISIBLE);
-            stepsSide.setVisibility(View.GONE);
-            showBack = false;
-        } else {
-            ingredientSide.setVisibility(View.GONE);
-            stepsSide.setVisibility(View.VISIBLE);
-            showBack = true;
-        }
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    public void onPause(){
-        if(currentStepTTS !=null){
-            currentStepTTS.stop();
-            currentStepTTS.shutdown();
-        }
-        sensorManager.unregisterListener(this);
-        super.onPause();
-    }
-
-    private class MyGestureListener implements GestureDetector.OnGestureListener {
-        @Override
-        public boolean onDown(MotionEvent e) {
-            Log.d(TAG, "onDown");
-            return true;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-            Log.d(TAG, "onShowPress");
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            Log.d(TAG, "onSingleTapUp");
-            return false;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            Log.d(TAG, "onScroll");
-            return false;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-            Log.d(TAG, "onLongPress");
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            Log.d(TAG, "onFling");
-            return false;
-        }
+    public boolean onDown(MotionEvent e) {
+      Log.d(TAG, "onDown");
+      return true;
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        float ax, ay, az;
-
-        ax = event.values[0];
-        ay = event.values[1];
-        az = event.values[2];
-
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            if(buttonPressed == true) {
-                long currTime = System.currentTimeMillis();
-                // check in intervals of 25 milliseconds
-                if ((currTime - lastUpdate) > 50) {
-                    long diffTime = (currTime - lastUpdate);
-                    lastUpdate = currTime;
-
-                    //float speed = Math.abs(ax + ay + az - prevX - prevY - prevZ) / diffTime * 10000;
-                    float speed = Math.abs(ax + az - prevX - prevZ) / diffTime * 10000;
-
-                    if (speed > SHAKE_THRESHOLD) {
-                        currentStepNum++;
-                        String toSpeak = steps.get(currentStepNum);
-                        currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
-
-                        Toast.makeText(this, "Device was shaken", Toast.LENGTH_SHORT).show();
-                        buttonPressed = false;
-                    }
-
-                    prevX = ax;
-                    prevY = ay;
-                    prevZ = az;
-                }
-            }
-        }
+    public void onShowPress(MotionEvent e) {
+      Log.d(TAG, "onShowPress");
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public boolean onSingleTapUp(MotionEvent e) {
+      Log.d(TAG, "onSingleTapUp");
+      return false;
     }
 
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+      Log.d(TAG, "onScroll");
+      return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+      Log.d(TAG, "onLongPress");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+      Log.d(TAG, "onFling");
+      return false;
+    }
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    float ax, ay, az;
+
+    ax = event.values[0];
+    ay = event.values[1];
+    az = event.values[2];
+
+    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+      if (buttonPressed == true) {
+        long currTime = System.currentTimeMillis();
+        // check in intervals of 25 milliseconds
+        if ((currTime - lastUpdate) > 50) {
+          long diffTime = (currTime - lastUpdate);
+          lastUpdate = currTime;
+
+          // float speed = Math.abs(ax + ay + az - prevX - prevY - prevZ) / diffTime * 10000;
+          float speed = Math.abs(ax + az - prevX - prevZ) / diffTime * 10000;
+
+          if (speed > SHAKE_THRESHOLD) {
+            currentStepNum++;
+            String toSpeak = steps.get(currentStepNum);
+            currentStepTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+
+            Toast.makeText(this, "Device was shaken", Toast.LENGTH_SHORT).show();
+            buttonPressed = false;
+          }
+
+          prevX = ax;
+          prevY = ay;
+          prevZ = az;
+        }
+      }
+    }
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 }
-
-
-
